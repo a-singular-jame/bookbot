097860d9bf12fb8f83e9964e4ef6b1362fbbb7e5
a-singular-jame <james.m.farrell@sky.com>
2025-07-08 15:48:51 +0100

    created a function in stats.py that counts the number of each character present inside of frankenstein.txt

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-from stats import num_words
+from stats import num_letters, num_words
 
 num_words("books/frankenstein.txt")
+num_letters("books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -7,3 +7,16 @@ def num_words(i):
     text = get_book_text(i)
     no_words = text.split()
     print(len(no_words), "words found in the document")
+
+def num_letters(n):
+    text = get_book_text(n)
+    text = text.lower()
+    letters_dict = {}
+    for letter in text:
+        if letter in letters_dict:
+            letters_dict[letter] += 1
+        else:
+            letters_dict[letter] = 1
+
+    for  letter, number in letters_dict.items():
+        print(f"'{letter}': {number}")
