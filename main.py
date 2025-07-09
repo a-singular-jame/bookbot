da06bd661c9f56eb31d566ec9203c5754b69b499
a-singular-jame <james.m.farrell@sky.com>
2025-07-09 03:54:57 +0100

    added comments and made the output more readable

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from stats import num_letters, num_words, filters
 
+
+
+print("============ BOOKBOT ============")
+print("Analyzing book found at books/frankenstein.txt...")
+print("----------- Word Count ----------")
+
 num_words("books/frankenstein.txt")
+
+print("--------- Character Count -------")
+
 num_letters("books/frankenstein.txt")
 filters()
+
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -1,15 +1,20 @@
 letters_dict = {}
 sorted_totals = []
 
+# opens and returns whole text file
+# for other functions to use
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+# finds and prints the number of words in given file
 def num_words(i):
     text = get_book_text(i)
     no_words = text.split()
     print(f"Found {len(no_words)} total words")
 
+# calculates and prints total of each 
+# character in given file
 def num_letters(n):
     text = get_book_text(n)
     text = text.lower()
@@ -20,6 +25,8 @@ def num_letters(n):
         else:
             letters_dict[letter] = 1
 
+# sorts and filters characters in 
+# correct order and only alphabetical
 def filters():
     for  letter, number in letters_dict.items():
         sorted_totals.append ({"char" : letter, "num" : number})
@@ -30,6 +37,7 @@ def filters():
         if temp.isalpha() == True:
             print(f"{sorted_totals[i]["char"]}: {sorted_totals[i]["num"]}")
 
+# apparently this is needed to sort properly
 def sort_on(items):
     return items["num"]
 
